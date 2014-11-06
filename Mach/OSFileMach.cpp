416173b8817f490829420195360bdc91bb08ab56
Richard Walters <rwalters@digitalstirling.com>
2014-11-05 23:06:52 -0800

    Clean up some of Mach implementation of OSFile

--- Mach/OSFileMach.cpp
+++ Mach/OSFileMach.cpp
@@ -1,13 +1,13 @@
 /**
- * @file FileSystemFileMach.cpp
+ * @file OSFileMach.cpp
  *
  * This module contains the Mac (e.g. Mac OS X) specific part of the
- * implementation of the FileSystemFile class.
+ * implementation of the Files::OSFile class.
  *
- * Copyright (c) 2013 by Richard Walters
+ * Copyright (c) 2013-2014 by Richard Walters
  */
 
-#include "../FileSystemFile.h"
+#include "../OSFile.h"
 
 #include <errno.h>
 #include <stddef.h>
@@ -15,31 +15,35 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-bool FileSystemFile::IsExisting() {
-    return (access(_path.c_str(), 0) == 0);
-}
+namespace Files {
 
-bool FileSystemFile::SetSize(size_t size) {
-    return (ftruncate(fileno(_handle), (off_t)size) == 0);
-}
-
-bool FileSystemFile::CreatePath(std::string path) {
-    const size_t delimiter = path.find_last_of("/\\");
-    if (delimiter == std::string::npos) {
-        return false;
+    bool OSFile::IsExisting() {
+        return (access(_path.c_str(), 0) == 0);
     }
-    const std::string oneLevelUp(path.substr(0, delimiter));
-    if (mkdir(oneLevelUp.c_str(), S_IRUSR | S_IWUSR | S_IXUSR) == 0) {
+
+    bool OSFile::SetSize(uint64_t size) {
+        return (ftruncate(fileno(_handle), (off_t)size) == 0);
+    }
+
+    bool OSFile::CreatePath(std::string path) {
+        const size_t delimiter = path.find_last_of("/\\");
+        if (delimiter == std::string::npos) {
+            return false;
+        }
+        const std::string oneLevelUp(path.substr(0, delimiter));
+        if (mkdir(oneLevelUp.c_str(), S_IRUSR | S_IWUSR | S_IXUSR) == 0) {
+            return true;
+        }
+        if (errno != ENOENT) {
+            return false;
+        }
+        if (!CreatePath(oneLevelUp)) {
+            return false;
+        }
+        if (mkdir(oneLevelUp.c_str(), S_IRUSR | S_IWUSR | S_IXUSR) != 0) {
+            return false;
+        }
         return true;
     }
-    if (errno != ENOENT) {
-        return false;
-    }
-    if (!CreatePath(oneLevelUp)) {
-        return false;
-    }
-    if (mkdir(oneLevelUp.c_str(), S_IRUSR | S_IWUSR | S_IXUSR) != 0) {
-        return false;
-    }
-    return true;
+
 }
