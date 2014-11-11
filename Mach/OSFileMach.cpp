4502c21e8b5e207a9ea1b55fc6f7041d95dfc9f8
Richard Walters <rwalters@digitalstirling.com>
2014-11-11 01:18:05 -0800

    Update Xcode project and fill in missing OSFile Mach methods

--- Mach/OSFileMach.cpp
+++ Mach/OSFileMach.cpp
@@ -12,19 +12,71 @@
 #include <dirent.h>
 #include <mach-o/dyld.h>
 #include <errno.h>
+#include <pwd.h>
 #include <StringExtensions/StringExtensions.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
+#include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <vector>
 
 namespace Files {
 
+    /**
+     * This is the Mach-specific state for the OSFile class.
+     */
+    struct OSFileImpl {
+        FILE* handle;
+    };
+
+    OSFile::OSFile(std::string path)
+        : _path(path)
+        , _impl(new OSFileImpl())
+    {
+    }
+
+    OSFile::~OSFile() {
+        Close();
+    }
+
     bool OSFile::IsExisting() {
         return (access(_path.c_str(), 0) == 0);
+    }
+
+    bool OSFile::Open() {
+        Close();
+        _impl->handle = fopen(_path.c_str(), "r+b");
+        return (_impl->handle != NULL);
+    }
+
+    void OSFile::Close() {
+        if (_impl->handle == NULL) {
+            return;
+        }
+        (void)fclose(_impl->handle);
+        _impl->handle = NULL;
+    }
+
+    bool OSFile::Create() {
+        Close();
+        _impl->handle = fopen(_path.c_str(), "w+b");
+        if (_impl->handle == NULL) {
+            if (!CreatePath(_path)) {
+                return false;
+            } else {
+                _impl->handle = fopen(_path.c_str(), "w+b");
+            }
+        }
+        return (_impl->handle != NULL);
+    }
+
+    void OSFile::Destroy() {
+        Close();
+        (void)remove(_path.c_str());
     }
 
     std::string OSFile::GetExeDirectory() {
@@ -71,7 +123,7 @@ namespace Files {
             directoryWithSeparator += '/';
         }
         list.clear();
-        DIR* dir = opendir(directory);
+        DIR* dir = opendir(directory.c_str());
         if (dir != NULL) {
             struct dirent entry;
             struct dirent* entryBack;
@@ -98,7 +150,7 @@ namespace Files {
             ) {
             directoryWithSeparator += '/';
         }
-        DIR* dir = opendir(directory);
+        DIR* dir = opendir(directory.c_str());
         if (dir != NULL) {
             struct dirent entry;
             struct dirent* entryBack;
@@ -125,12 +177,77 @@ namespace Files {
                 }
             }
             (void)closedir(dir);
-            (void)rmdir(directory);
+            (void)rmdir(directory.c_str());
         }
     }
 
+    uint64_t OSFile::GetSize() const {
+        if (_impl->handle == NULL) {
+            return 0;
+        }
+        const long originalPosition = ftell(_impl->handle);
+        if (originalPosition == EOF) {
+            return 0;
+        }
+        if (fseek(_impl->handle, 0, SEEK_END) == EOF) {
+            (void)fseek(_impl->handle, originalPosition, SEEK_SET);
+            return 0;
+        }
+        const long endPosition = ftell(_impl->handle);
+        (void)fseek(_impl->handle, originalPosition, SEEK_SET);
+        if (endPosition == EOF) {
+            return 0;
+        }
+        return (uint64_t)endPosition;
+    }
+
     bool OSFile::SetSize(uint64_t size) {
-        return (ftruncate(fileno(_handle), (off_t)size) == 0);
+        return (ftruncate(fileno(_impl->handle), (off_t)size) == 0);
+    }
+
+    uint64_t OSFile::GetPosition() const {
+        if (_impl->handle == NULL) {
+            return 0;
+        }
+        const long position = ftell(_impl->handle);
+        if (position == EOF) {
+            return 0;
+        }
+        return (uint64_t)position;
+    }
+
+    void OSFile::SetPosition(uint64_t position) {
+        if (_impl->handle == NULL) {
+            return;
+        }
+        (void)fseek(_impl->handle, (long)position, SEEK_SET);
+    }
+
+    size_t OSFile::Peek(void* buffer, size_t numBytes) const {
+        if (_impl->handle == NULL) {
+            return 0;
+        }
+        const long originalPosition = ftell(_impl->handle);
+        if (originalPosition == EOF) {
+            return 0;
+        }
+        const size_t readResult = fread(buffer, 1, numBytes, _impl->handle);
+        (void)fseek(_impl->handle, originalPosition, SEEK_SET);
+        return readResult;
+    }
+
+    size_t OSFile::Read(void* buffer, size_t numBytes) {
+        if (_impl->handle == NULL) {
+            return 0;
+        }
+        return fread(buffer, 1, numBytes, _impl->handle);
+    }
+
+    size_t OSFile::Write(const void* buffer, size_t numBytes) {
+        if (_impl->handle == NULL) {
+            return 0;
+        }
+        return fwrite(buffer, 1, numBytes, _impl->handle);
     }
 
     bool OSFile::CreatePath(std::string path) {
