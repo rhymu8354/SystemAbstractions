4502ecd314cbcab51e34278fd1d896ed573aa1c1
Richard Walters <rwalters@digitalstirling.com>
2016-05-26 14:38:24 -0700

    Update Xcode project and add DirectoryMonitorMach.cpp

---
+++ Mach/DirectoryMonitorMach.cpp
@@ -0,0 +1,143 @@
+/**
+ * @file DirectoryMonitorMach.cpp
+ *
+ * This module contains the Mach implementation of the
+ * SystemAbstractions::DirectoryMonitor class.
+ *
+ * Copyright (c) 2016 by Richard Walters
+ */
+
+#include "../DirectoryMonitor.hpp"
+#include "../Posix/PipeSignal.hpp"
+
+#include <assert.h>
+#include <fcntl.h>
+#include <sys/event.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <thread>
+#include <unistd.h>
+
+namespace SystemAbstractions {
+
+    /**
+     * This structure contains the private methods and properties of
+     * the DirectoryMonitor class.
+     */
+    struct DirectoryMonitorImpl {
+        // Properties
+
+        /**
+         * @todo Needs documentation
+         */
+        std::thread worker;
+
+        /**
+         * @todo Needs documentation
+         */
+        DirectoryMonitor::Owner* owner;
+
+        /**
+         * @todo Needs documentation
+         */
+        PipeSignal stopSignal;
+
+        /**
+         * @todo Needs documentation
+         */
+        int dirHandle = -1;
+
+        /**
+         * @todo Needs documentation
+         */
+        int kqueueHandle = -1;
+
+        /**
+         * @todo Needs documentation
+         */
+
+        // Methods
+
+        /**
+         * @todo Needs documentation
+         */
+        void Run() {
+            struct kevent kqueueEvents[2];
+            EV_SET(&kqueueEvents[0], stopSignal.GetSelectHandle(), EVFILT_READ, EV_ADD, 0, 0, NULL);
+            EV_SET(&kqueueEvents[1], dirHandle, EVFILT_VNODE, EV_ADD, NOTE_WRITE, 0, NULL);
+            struct kevent change;
+            for (;;) {
+                if (kevent(kqueueHandle, &change, 1, kqueueEvents, 2, NULL) < 0) {
+                    break;
+                }
+                if (change.ident == stopSignal.GetSelectHandle()) {
+                    break;
+                }
+                owner->DirectoryMonitorChangeDetected();
+            }
+        }
+    };
+
+    DirectoryMonitor::DirectoryMonitor()
+        : _impl(new DirectoryMonitorImpl())
+    {
+    }
+
+
+    DirectoryMonitor::DirectoryMonitor(DirectoryMonitor&& other)
+        : _impl(std::move(other._impl))
+    {
+    }
+
+    DirectoryMonitor::DirectoryMonitor(std::unique_ptr< DirectoryMonitorImpl >&& impl)
+        : _impl(std::move(impl))
+    {
+    }
+
+    DirectoryMonitor::~DirectoryMonitor() {
+        Stop();
+    }
+
+    DirectoryMonitor& DirectoryMonitor::operator=(DirectoryMonitor&& other) {
+        assert(this != &other);
+        _impl = std::move(other._impl);
+        return *this;
+    }
+
+    bool DirectoryMonitor::Start(
+        Owner* owner,
+        const std::string& path
+    ) {
+        Stop();
+        if (!_impl->stopSignal.Initialize()) {
+            return false;
+        }
+        _impl->dirHandle = open(path.c_str(), O_EVTONLY);
+        if (_impl->dirHandle < 0) {
+            return false;
+        }
+        _impl->stopSignal.Clear();
+        _impl->owner = owner;
+        _impl->kqueueHandle = kqueue();
+        if (_impl->kqueueHandle < 0) {
+            (void)close(_impl->dirHandle);
+            _impl->dirHandle = -1;
+            return false;
+        }
+        _impl->worker = std::move(std::thread(&DirectoryMonitorImpl::Run, _impl.get()));
+        return true;
+    }
+    
+    void DirectoryMonitor::Stop() {
+        if (!_impl->worker.joinable()) {
+            return;
+        }
+        _impl->stopSignal.Set();
+        _impl->worker.join();
+        (void)close(_impl->kqueueHandle);
+        _impl->kqueueHandle = -1;
+        (void)close(_impl->dirHandle);
+        _impl->dirHandle = -1;
+    }
+    
+}
