e152391e1ec572063f095556fba576d6db59c2f2
Richard Walters <rwalters@digitalstirling.com>
2017-09-28 00:12:18 -0700

    Add empty implementation of clipboard for Linux

---
+++ Linux/ClipboardLinux.cpp
@@ -0,0 +1,39 @@
+/**
+ * @file ClipboardLinux.cpp
+ *
+ * This module contains the Linux specific part of the 
+ * implementation of the SystemAbstractions::Clipboard class.
+ *
+ * Copyright (c) 2017 by Richard Walters
+ */
+
+#include "../Clipboard.hpp"
+
+namespace SystemAbstractions {
+
+    /**
+     * This is the Linux-specific state for the Clipboard class.
+     */
+    struct Clipboard::Impl {
+    };
+
+    Clipboard::Clipboard()
+        : impl_(new Impl())
+    {
+    }
+
+    Clipboard::~Clipboard() {
+    }
+
+    void Clipboard::Copy(const std::string& s) {
+    }
+
+    bool Clipboard::HasString() {
+        return false;
+    }
+
+    std::string Clipboard::PasteString() {
+        return "";
+    }
+
+}
