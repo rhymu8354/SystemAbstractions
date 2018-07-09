48d01bbaf9bc5a2261da4a04e821f35187aca911
Richard Walters <rwalters@digitalstirling.com>
2018-07-08 17:29:36 -0700

    Establish test case for StringFile

---
+++ test/src/StringFileTests.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file StringFileTests.cpp
+ *
+ * This module contains the unit tests of the
+ * SystemAbstractions::StringFile class.
+ *
+ * © 2018 by Richard Walters
+ */
+
+#include <gtest/gtest.h>
+#include <stdint.h>
+#include <SystemAbstractions/StringFile.hpp>
+#include <vector>
+
+TEST(StringFileTests, Placeholder) {
+}
