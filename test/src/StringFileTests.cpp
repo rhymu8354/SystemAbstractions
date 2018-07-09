711d7614f47445924c37b58c11ed0662eaf4beb5
Richard Walters <rwalters@digitalstirling.com>
2018-07-08 18:30:19 -0700

    Test the remainder of the public methods of StringFile

--- test/src/StringFileTests.cpp
+++ test/src/StringFileTests.cpp
@@ -237,3 +237,100 @@ TEST(StringFileTests, WriteBeyondEndAndIntoMiddle) {
         buffer
     );
 }
+
+TEST(StringFileTests, ConstructFromString) {
+    const std::string testString = "Hello, World!\r\n";
+    SystemAbstractions::StringFile sf(testString);
+    SystemAbstractions::IFile::Buffer buffer(testString.length());
+    ASSERT_EQ(
+        testString.length(),
+        sf.Read(buffer)
+    );
+    ASSERT_EQ(
+        testString,
+        std::string(
+            buffer.begin(),
+            buffer.end()
+        )
+    );
+}
+
+TEST(StringFileTests, ConstructFromVector) {
+    std::vector< uint8_t > testVector{'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\r', '\n'};
+    SystemAbstractions::StringFile sf(testVector);
+    SystemAbstractions::IFile::Buffer buffer(testVector.size());
+    ASSERT_EQ(
+        testVector.size(),
+        sf.Read(buffer)
+    );
+    ASSERT_EQ(
+        testVector,
+        buffer
+    );
+}
+
+TEST(StringFileTests, AssignFromString) {
+    const std::string testString = "Hello, World!\r\n";
+    SystemAbstractions::StringFile sf;
+    sf = testString;
+    SystemAbstractions::IFile::Buffer buffer(testString.length());
+    ASSERT_EQ(
+        testString.length(),
+        sf.Read(buffer)
+    );
+    ASSERT_EQ(
+        testString,
+        std::string(
+            buffer.begin(),
+            buffer.end()
+        )
+    );
+}
+
+TEST(StringFileTests, AssignFromVector) {
+    std::vector< uint8_t > testVector{'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\r', '\n'};
+    SystemAbstractions::StringFile sf;
+    sf = testVector;
+    SystemAbstractions::IFile::Buffer buffer(testVector.size());
+    ASSERT_EQ(
+        testVector.size(),
+        sf.Read(buffer)
+    );
+    ASSERT_EQ(
+        testVector,
+        buffer
+    );
+}
+
+TEST(StringFileTests, TypeCastToString) {
+    const std::string testString = "Hello, World!\r\n";
+    SystemAbstractions::StringFile sf(testString);
+    ASSERT_EQ(testString, (std::string)sf);
+}
+
+TEST(StringFileTests, TypeCastToVector) {
+    std::vector< uint8_t > testVector{'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\r', '\n'};
+    SystemAbstractions::StringFile sf(testVector);
+    ASSERT_EQ(testVector, (std::vector< uint8_t >)sf);
+}
+
+TEST(StringFileTests, Remove) {
+    const std::string testString = "Hello, World!\r\n";
+    SystemAbstractions::StringFile sf(testString);
+    sf.SetPosition(5);
+    sf.Remove(0);
+    ASSERT_EQ(testString.length(), sf.GetSize());
+    ASSERT_EQ(5, sf.GetPosition());
+    sf.Remove(2);
+    ASSERT_EQ(testString.length() - 2, sf.GetSize());
+    ASSERT_EQ(3, sf.GetPosition());
+    ASSERT_EQ("llo, World!\r\n", (std::string)sf);
+    sf.Remove(5);
+    ASSERT_EQ(testString.length() - 7, sf.GetSize());
+    ASSERT_EQ(0, sf.GetPosition());
+    ASSERT_EQ("World!\r\n", (std::string)sf);
+    sf.Remove(10);
+    ASSERT_EQ(0, sf.GetSize());
+    ASSERT_EQ(0, sf.GetPosition());
+    ASSERT_EQ("", (std::string)sf);
+}
