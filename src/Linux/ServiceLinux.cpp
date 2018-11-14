d2e2da5348f86d6d8ec2f7d9a9a8b1b09e88f071
Richard Walters <rwalters@digitalstirling.com>
2018-11-14 11:29:28 -0800

    Implement ServiceLinux based on receiving SIGTERM to stop.

--- src/Linux/ServiceLinux.cpp
+++ src/Linux/ServiceLinux.cpp
@@ -7,7 +7,31 @@
  * © 2018 by Richard Walters
  */
 
+#include <signal.h>
 #include <SystemAbstractions/Service.hpp>
+#include <thread>
+
+namespace {
+
+    /**
+     * This flag indicates whether or not the service should shut down.
+     */
+    bool shutDown = false;
+
+    /**
+     * This function is set up to be called when the SIGTERM signal is
+     * received by the program.  It just sets the "shutDown" flag
+     * and relies on the service to be polling the flag to detect
+     * when it's been set.
+     *
+     * @param[in] sig
+     *     This is the signal for which this function was called.
+     */
+    void InterruptHandler(int) {
+        shutDown = true;
+    }
+
+}
 
 namespace SystemAbstractions {
 
@@ -16,6 +40,26 @@ namespace SystemAbstractions {
      * the Service class.
      */
     struct Service::Impl {
+        /**
+         * This points back to the instance's interface.
+         */
+        Service* instance;
+
+        /**
+         * This runs PollShutdown as a worker thread.
+         */
+        std::thread worker;
+
+        /**
+         * This method runs as a worker thread, polling the "shutdown" flag
+         * until it's set or the thread is told to stop.
+         */
+        void PollShutdown() {
+            while (!shutDown) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            }
+            instance->Stop();
+        }
     };
 
     Service::~Service() noexcept = default;
@@ -25,10 +69,16 @@ namespace SystemAbstractions {
     Service::Service()
         : impl_(new Impl())
     {
+        impl_->instance = this;
     }
 
     int Service::Start() {
-        return 0;
+        const auto previousInterruptHandler = signal(SIGTERM, InterruptHandler);
+        impl_->worker = std::thread(&Impl::PollShutdown, impl_.get());
+        const int result = Run();
+        impl_->worker.join();
+        (void)signal(SIGTERM, previousInterruptHandler);
+        return result;
     }
 
 }
